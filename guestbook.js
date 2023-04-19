8e3c4d9375b0a4760c1b963ae651c57dfcc5624b
zishan sheikh <46299601+thesheikhzishan@users.noreply.github.com>
2023-04-19 13:45:30 +0530

    Updated the api url

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/90e78744-6b94-479a-a4a4-da5f6709e67e/guestbook/read-guestbook-entries-sequence';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
